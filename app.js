8b5ba147782bef0b3c564946f391a6b19ba5e900
josephrajp <mailjosephraj@gmail.com>
2017-07-28 20:59:07 +0530

    updated..

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ let temp;
 
 const imgNumbers = new Image();
 imgNumbers.src = '/images/numbers.jpg';
+imgNumbers.id = 'numbers';
 const imgTiger = new Image();
 imgTiger.src = '/images/tiger.jpg';
+imgTiger.id = 'tiger';
 const imgPeacock = new Image();
-imgPeacock.src = '/images/peacock.jpg'
+imgPeacock.src = '/images/peacock.jpg';
+imgPeacock.id = 'peacock';
 const imgPuppy = new Image();
 imgPuppy.src = '/images/puppy.jpg';
+imgPuppy.id = 'puppy';
 
 function selScreen(){
   ctx.fillStyle = '#fff';
@@ -46,20 +50,37 @@ function selScreen(){
     for(var i = 0; i < imgToDraw.length; i++){
       var b = imgToDraw[i];
       if(mX >= b.x && mX < b.x+b.w && mY >= b.y && mY < b.y+b.h){
-        initDrawImg(b.img);
+        ctx.canvas.removeEventListener('mousedown', selImg);
+        gameCanvas(b.img);
       }
     }
   }
-
-  function initDrawImg(img){
-    console.log(img);
-    ctx.canvas.removeEventListener('mousedown', selImg);
-  }
 }
 
-
-
 function gameCanvas(img) {
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(0, 0, cW, cH);
+  addShuffleButton();
+  eventShuffle();
+
+  var imgBlocks = [
+    [300, 300],
+    [0, 0],
+    [100, 0],
+    [200, 0],
+    [300, 0],
+    [0, 100],
+    [100, 100],
+    [200, 100],
+    [300, 100],
+    [0, 200],
+    [100, 200],
+    [200, 200],
+    [300, 200],
+    [0, 300],
+    [100, 300],
+    [200, 300]
+  ];
 
   const blocks = [
     new Block(300, 300, 16),
@@ -79,15 +100,38 @@ function gameCanvas(img) {
     new Block(100, 300, 14),
     new Block(200, 300, 15)
   ];
-
-  moveRandom(700);
-
+  
   //Canvas event Listener
-  ctx.canvas.addEventListener('click', function (evt) {
-    moveClicked(evt.offsetX, evt.offsetY, blocks)
-    drawBlocks(blocks);
-    setMoveable(blocks);
-  });
+  function addEventToMove(){
+    ctx.canvas.addEventListener('mousedown', function (evt) {
+      moveClicked(evt.offsetX, evt.offsetY, blocks)
+      drawBlocks(blocks);
+      setMoveable(blocks);
+    });
+  }
+
+  function eventShuffle(){
+    ctx.canvas.addEventListener('mousedown', offEvent);
+    function offEvent(evt){
+      var mX = evt.offsetX;
+      var mY = evt.offsetY;
+      if(mX >= 125 && mX < 275 && mY >= 425 && mY < 475){
+        ctx.canvas.removeEventListener('mousedown', offEvent);
+        moveRandom(700);
+        addEventToMove();
+      }
+    }
+  }
+
+  function addShuffleButton(){
+    ctx.fillStyle = '#d33';
+    ctx.fillRect(125, 425, 150, 50);
+    ctx.fillStyle = '#fff';
+    ctx.font = '30px sans-serif';
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
+    ctx.fillText('Shuffle', 200, 450);
+  }
 
   //To shuffle
   function moveRandom(num) {
@@ -174,21 +218,26 @@ function gameCanvas(img) {
 
   // Draw each block
   function drawBlock(block) {
+    var id = block.id;
     if (block.id != 16) {
       ctx.fillStyle = block.col;
       ctx.fillRect(block.x, block.y, block.w, block.h);
-      ctx.strokeStyle = 'rgba(0, 0, 0, .1)';
-      ctx.strokeRect(block.x, block.y, block.w, block.h);
-      ctx.strokeStyle = 'rgba(0, 0, 0, .1)';
-      ctx.strokeRect(block.x + 1, block.y + 1, block.w - 2, block.h - 2);
-      ctx.stroke();
-      ctx.fillStyle = 'rgba(0, 0, 0, .5)';
-      ctx.font = '48px sans-serif';
-      ctx.textBaseline = 'middle';
-      ctx.textAlign = 'center';
-      ctx.fillText(block.id, block.x + 50, block.y + 50);
+      if(img.id === 'numbers'){
+        ctx.fillStyle = 'rgba(0, 0, 0, .5)';
+        ctx.font = '48px sans-serif';
+        ctx.textBaseline = 'middle';
+        ctx.textAlign = 'center';
+        ctx.fillText(block.id, block.x + 50, block.y + 50);
+      }else{
+        ctx.drawImage(img, imgBlocks[id][0], imgBlocks[id][1], block.w, block.h, block.x, block.y, block.w, block.h);
+      }
+        ctx.strokeStyle = 'rgba(0, 0, 0, .2)';
+        ctx.strokeRect(block.x, block.y, block.w, block.h);
+        ctx.strokeStyle = 'rgba(0, 0, 0, .2)';
+        ctx.strokeRect(block.x + 1, block.y + 1, block.w - 2, block.h - 2);
+        ctx.stroke();
     } else {
-      ctx.fillStyle = '#000';
+      ctx.fillStyle = '#7d7d7d';
       ctx.fillRect(block.x, block.y, block.w, block.h);
     }
   }
@@ -199,21 +248,6 @@ function gameCanvas(img) {
   }
 
 } // initCanvas
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 window.addEventListener('load', function (evt) {
   selScreen();
-  // gameCanvas();
 });
